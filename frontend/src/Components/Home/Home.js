8177e88fbb493974fbd888a1baaaf9e59816224e
tdoshi <tn716378@dal.ca>
2024-10-22 21:39:04 -0300

    changes: home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Home : latest?</h1>
       <p>This is the homepage of your application.</p>
     </div>
   );
