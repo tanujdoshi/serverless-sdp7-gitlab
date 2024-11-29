2257d87d332cf8d8573cff70660bec7128a6bdc7
Jay Patel <jy432150@dal.ca>
2024-11-28 21:30:09 -0400

    Added states and submit button in feedBack form

--- frontend/src/Components/DataAnalysis/feedBack.js
+++ frontend/src/Components/DataAnalysis/feedBack.js
@@ -1,8 +1,15 @@
 import {React} from "react";
 
 const FeedbackForm = () => {
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState("");
+  const [feedback, setFeedback] = useState("");
+
     return(
       <div style={{padding:"40px"}}>
+          <Button variant="contained" color="primary" onClick={handleSubmit} fullWidth>
+          Submit
+        </Button>
       </div>
     )
 
