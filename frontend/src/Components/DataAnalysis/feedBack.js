cd9010da43647a958c98268d156e6b8f769309a4
Jay Patel <jy432150@dal.ca>
2024-11-28 21:37:23 -0400

    Added functionality to get the data processing tasks from cloud run function.

--- frontend/src/Components/DataAnalysis/feedBack.js
+++ frontend/src/Components/DataAnalysis/feedBack.js
@@ -1,37 +1,64 @@
-import {React} from "react";
+import { React } from "react";
 
 const FeedbackForm = () => {
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState("");
   const [feedback, setFeedback] = useState("");
 
-  const handleSubmit = (e) => {  
-    e.preventDefault();
-    const {process_id, filename, type} = selectedTask;
+  const userEmail = localStorage.getItem("userEmail");
+  useEffect(() => {
     axios
-    .post("https://us-central1-serverless-pro-442123.cloudfunctions.net/feedd3", {
-      userEmail: userEmail,
-      process_id,
-      fileName:filename,
-      dpType:type,
-      feed:feedback,
-    },{
-        headers: {
-            "Content-Type": "application/json",
+      .get(
+        "https://kdhprlykjeacymoqef22co3ie40unnqa.lambda-url.us-east-1.on.aws/",
+        { userEmail },
+        {
+          headers: {
+            "Content-Type": "application/json", // Set only if necessary
+          },
+        }
+      )
+      .then((res) => {
+        setTasks(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { process_id, filename, type } = selectedTask;
+    axios
+      .post(
+        "https://us-central1-serverless-pro-442123.cloudfunctions.net/feedd3",
+        {
+          userEmail: userEmail,
+          process_id,
+          fileName: filename,
+          dpType: type,
+          feed: feedback,
         },
-    })
-    .then(() => alert("Feedback submitted!"))
-    .catch((error) => console.error(error));
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(() => alert("Feedback submitted!"))
+      .catch((error) => console.error(error));
   };
 
-    return(
-      <div style={{padding:"40px"}}>
-          <Button variant="contained" color="primary" onClick={handleSubmit} fullWidth>
-          Submit
-        </Button>
-      </div>
-    )
-
-}
+  return (
+    <div style={{ padding: "40px" }}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+        fullWidth
+      >
+        Submit
+      </Button>
+    </div>
+  );
+};
 
 export default FeedbackForm;
