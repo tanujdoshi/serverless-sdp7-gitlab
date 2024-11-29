297d10877067141188653c5201ca4d93147f1a25
Jay Patel <jy432150@dal.ca>
2024-11-28 21:32:23 -0400

    Added handleSubmit func to submit .

--- frontend/src/Components/DataAnalysis/feedBack.js
+++ frontend/src/Components/DataAnalysis/feedBack.js
@@ -5,6 +5,25 @@ const FeedbackForm = () => {
   const [selectedTask, setSelectedTask] = useState("");
   const [feedback, setFeedback] = useState("");
 
+  const handleSubmit = (e) => {  
+    e.preventDefault();
+    const {process_id, filename, type} = selectedTask;
+    axios
+    .post("https://us-central1-serverless-pro-442123.cloudfunctions.net/feedd3", {
+      userEmail: userEmail,
+      process_id,
+      fileName:filename,
+      dpType:type,
+      feed:feedback,
+    },{
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    .then(() => alert("Feedback submitted!"))
+    .catch((error) => console.error(error));
+  };
+
     return(
       <div style={{padding:"40px"}}>
           <Button variant="contained" color="primary" onClick={handleSubmit} fullWidth>
