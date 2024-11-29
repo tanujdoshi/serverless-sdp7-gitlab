cb91a6ba76de5ef3c6895e07df536277f5f2badb
Jay Patel <jy432150@dal.ca>
2024-11-29 16:23:58 -0400

    Added useState to get the feedback data from cloud run.

--- frontend/src/Components/DataAnalysis/feedbackTable.js
+++ frontend/src/Components/DataAnalysis/feedbackTable.js
@@ -1,6 +1,19 @@
 import React from "react";
 
+
 const FeedbackTable = () => {
+
+  const [feedbackData, setFeedbackData] = useState([]);
+
+  useEffect(()=>{
+    console.log("Feedback Data");
+    console.log(feedbackData);
+    axios.get("https://getfeedbackdetails-710015716338.us-central1.run.app")
+    .then((res)=>{
+        setFeedbackData(res.data);
+    })
+    .catch((err)=>{ console.log(err); });
+  },[])
   return (
     <h1>Feedback Table</h1>
   );
