497d53a8fca86b5c70b4b9e9914664cc36770490
tdoshi <tn716378@dal.ca>
2024-12-01 17:59:51 -0400

    UI changes

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 // Navbar.js
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Button } from "@mui/material";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../Context/UserContext";
 
@@ -14,48 +14,50 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        {isLoggedIn ? (
-          <>
-            <Button color="inherit" component={Link} to="/txt-transform">
-              txt-transform
-            </Button>
-            <Button color="inherit" component={Link} to="/json-to-csv">
-              json-to-csv
-            </Button>
-            <Button color="inherit" component={Link} to="/word-cloud">
-              Word cloud
-            </Button>
-            <Button color="inherit" component={Link} to="/feedbackform">
-              DP Feedback Form
-            </Button>
-            <Button color="inherit" component={Link} to="/feedbacktable">
-            DP Feedback Table
-            </Button>
-            <Button color="inherit" component={Link} to="/admin">
-              Admin
-            </Button>
-            <Button color="inherit" component={Link} to="/virtual-assistant">
-              Virtual Assistant
-            </Button>
-            <Button color="inherit" onClick={onLogout}>
-              Logout
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={Link} to="/signup">
-              Sign Up
-            </Button>
-          </>
-        )}
+    <AppBar position="static" sx={{ backgroundColor: "#1976d2" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box>
+          <Button color="inherit" component={Link} to="/" sx={{ fontSize: 16 }}>
+            Home
+          </Button>
+        </Box>
+        <Box>
+          {isLoggedIn ? (
+            <>
+              <Button color="inherit" component={Link} to="/feedbackform">
+                DP Feedback Form
+              </Button>
+              <Button color="inherit" component={Link} to="/feedbacktable">
+                DP Feedback Table
+              </Button>
+              <Button color="inherit" component={Link} to="/admin">
+                Admin
+              </Button>
+              <Button color="inherit" component={Link} to="/virtual-assistant">
+                Virtual Assistant
+              </Button>
+              <Button
+                color="inherit"
+                onClick={onLogout}
+                sx={{
+                  backgroundColor: "#e57373",
+                  ":hover": { backgroundColor: "#ef5350" },
+                }}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Sign Up
+              </Button>
+            </>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
